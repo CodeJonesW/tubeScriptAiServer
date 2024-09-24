fc57cbee8852f80e879f92629b08f77a3d59d926
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2024-09-24 13:22:12 -0500

    raise exception in youtube download fn

--- services/youtube_service.py
+++ services/youtube_service.py
@@ -20,18 +20,21 @@ def download_audio(youtube_url):
             'preferredquality': '192',
         }],
     }
+    try:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(youtube_url, download=True)
+            logger.info(f"yt-dlp info_dict: {info_dict}")
+            video_id = info_dict.get("id", None)
+            audio_file = os.path.join(output_dir, f"{video_id}.mp3")
+            logger.info(f"Audio file: {audio_file}")
 
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        info_dict = ydl.extract_info(youtube_url, download=True)
-        logger.info(f"yt-dlp info_dict: {info_dict}")
-        video_id = info_dict.get("id", None)
-        audio_file = os.path.join(output_dir, f"{video_id}.mp3")
-        logger.info(f"Audio file: {audio_file}")
+        if not os.path.exists(audio_file):
+            raise FileNotFoundError("Audio file could not be created")
 
-    if not os.path.exists(audio_file):
-        raise FileNotFoundError("Audio file could not be created")
-
-    return audio_file
+        return audio_file
+    except Exception as e:
+        logger.error(f"Error downloading audio: {str(e)}")
+        raise e
 
 def get_audio_duration(audio_file):
     probe = ffmpeg.probe(audio_file)
--- tasks.py
+++ tasks.py
@@ -16,10 +16,8 @@ def download_and_process(self, url, prompt, user_id):
     logger.info('Starting task ---- download_and_process')
     """Handles downloading, transcribing, and analyzing the video asynchronously."""
     try:
-        # You can still access the Flask app context from within the task using the same approach
-        from main import app  # Import the app here
+        from main import app
         with app.app_context():
-            # Get the user from the database
             user = User.query.get(user_id)
             if not user:
                 raise Exception("User not found")
