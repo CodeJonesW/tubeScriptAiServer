8cfac244bb52d11277bca1780d0b32311f937311
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2024-09-24 19:42:48 -0500

    fix typo in analyze text service

--- services/analyze_text_service.py
+++ services/analyze_text_service.py
@@ -20,9 +20,9 @@ def analyze_text(transcript, user_prompt):
     """Analyze the transcript based on the user's prompt using OpenAI GPT."""
     try:
         if user_prompt == 'summarize':
-            prompt = f'write me a detailed blog article about the follow text. Expand the length of the blog relative to the length of the content. Be detail oriented. Begin: {transcript}',
+            prompt = f'write me a detailed blog article about the follow text. Expand the length of the blog relative to the length of the content. Be detail oriented. Begin: {transcript}'
         else:
-            prompt = f"Follow this command {user_prompt} in regards to the following transcript\n\nTranscript:\n{transcript}"
+            prompt = f"Follow this command {user_prompt} in regards to this transcript\n\nTranscript:\n{transcript}"
 
         chat_completion = client.chat.completions.create(
             messages=[
@@ -31,8 +31,8 @@ def analyze_text(transcript, user_prompt):
                     "content": prompt
                 }
             ],
-            model="gpt-3.5-turbo",
-            )
+            model="gpt-3.5-turbo"
+        )
         return chat_completion.choices[0].message.content
 
     except Exception as e:
